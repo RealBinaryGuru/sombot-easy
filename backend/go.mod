637936c9f090583054d71a49a74d90f826a611b1
SEANGLAY <devolutionary.wizard@gmail.com>
2024-11-27 11:01:20 +0700

    add middleware log

--- backend/go.mod
+++ backend/go.mod
@@ -3,3 +3,9 @@ module github.com/RealBinaryGuru/sombot-easy
 go 1.22.2
 
 require github.com/go-chi/chi/v5 v5.1.0
+
+require (
+	github.com/samber/slog-chi v1.12.3 // indirect
+	go.opentelemetry.io/otel v1.29.0 // indirect
+	go.opentelemetry.io/otel/trace v1.29.0 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -1,25 +1,34 @@
 package main
 
 import (
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	slogchi "github.com/samber/slog-chi"
 )
 
 func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	// Set up the router
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+	r.Use(slogchi.New(logger)) // Middleware to use slog for request logging
+	r.Use(middleware.Recoverer)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("OK"))
+		w.Write([]byte("Hello, World!"))
+	})
+	r.Get("/error", func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, http.StatusText(400), 400)
 	})
 
 	port := ":3000"
-	log.Printf("Starting server on %s\n", port)
+	logger.Info("Starting server", slog.String("port", port))
 
 	if err := http.ListenAndServe(port, r); err != nil {
-		log.Fatalf("Server failed to start: %v", err)
+		logger.Error("Server failed to start", slog.String("error", err.Error()))
 	}
 }
