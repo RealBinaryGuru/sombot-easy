56c6a112fbab48e4e8ac7efa91ab440fecb7c294
SEANGLAY <devolutionary.wizard@gmail.com>
2024-12-12 16:25:12 +0700

    initial backend and docker

---
+++ api/src/main/java/com/seanglay/sombot_easy/SombotEasyApplication.java
@@ -0,0 +1,13 @@
+package com.seanglay.sombot_easy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SombotEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SombotEasyApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/seanglay/sombot_easy/SombotEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seanglay.sombot_easy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SombotEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
